d6a97c962c03a3e8200c2fb8a4189940c8af5295
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-18 20:04:30 -0300

    adciona exercicio

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt ("Escreva seu nome");
+let sobrenome = prompt ("Escreva seu sobrenome");
+
+console.log
+    ("Nome completo: " + nome + " " + sobrenome);
+
+console.log
+    ("Nome de catálogo: " + sobrenome.toLocaleUpperCase() + ", " + nome);
