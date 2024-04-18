e6c2f2c2a77a54f82248707381e6a7016602ba66
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-18 20:22:32 -0300

    adciona alerta

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let nome = prompt ("Escreva seu nome");
 let sobrenome = prompt ("Escreva seu sobrenome");
 
+// No console
+
 console.log
     ("Nome completo: " + nome + " " + sobrenome);
 
 console.log
     ("Nome de catálogo: " + sobrenome.toLocaleUpperCase() + ", " + nome);
+
+// No alerta
+
+alert
+    ("Nome completo: " + nome + " " + sobrenome);
+    
+alert
+    ("Nome de catálogo: " + sobrenome.toLocaleUpperCase() + ", " + nome);
+
